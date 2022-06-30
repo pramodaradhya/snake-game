6b1638e88efdbb3d90d61597eee359ef4f4c7cf9
pramodaradhya <46969756+pramodaradhya@users.noreply.github.com>
2022-06-30 19:37:05 +0530

    boundaries

---
+++ 2dsnakegame/Assets/Scripts/Boundaries.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+public class Boundaries : MonoBehaviour
+{
+	[SerializeField]
+	GameObject snake;
+	float xPositionWarp;
+	float yPositionWarp;
+	Transform snakePosition;
+	float addValue = 1f;
+	private void Start()
+	{
+		xPositionWarp = 21f;
+		yPositionWarp = 10f;
+	}
+
+	private void Update()
+	{
+		SnakeWarping();
+	}
+
+	private void SnakeWarping()
+	{
+		snakePosition = snake.transform;
+		if (snakePosition.transform.position.x > xPositionWarp)
+		{
+			snakePosition.transform.position = new Vector3(-snakePosition.transform.position.x + addValue, snakePosition.transform.position.y, 0.0f);
+		}
+
+		if (snakePosition.transform.position.x < -xPositionWarp)
+		{
+			snakePosition.transform.position = new Vector3(-snakePosition.transform.position.x - addValue, snakePosition.transform.position.y, 0.0f);
+		}
+
+		if (snakePosition.transform.position.y > yPositionWarp)
+		{
+			snakePosition.transform.position = new Vector3(snakePosition.transform.position.x, -snakePosition.transform.position.y + addValue, 0.0f);
+		}
+
+		if (snakePosition.transform.position.y < -yPositionWarp)
+		{
+			snakePosition.transform.position = new Vector3(snakePosition.transform.position.x, -snakePosition.transform.position.y - addValue, 0.0f);
+		}
+	}
+}
